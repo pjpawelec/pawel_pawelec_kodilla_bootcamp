bda6d9247c3845d80a37f13b0edb9eb50a46ceac
pjpawelec <pj.pawelec@gmail.com>
2020-02-15 17:35:07 +0100

    6.1 commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result =  simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int additionResult = calculator.add(3,2);
+        if(additionResult == 5) {
+            System.out.println("test dodawania OK");
+        }else{
+            System.out.println("dodawanie error!");
+        }
+
+        int subtractionResult = calculator.subtract(3,2);
+        if(subtractionResult == 1) {
+            System.out.println("test odejmowania OK");
+        }else{
+            System.out.println("odejmowanie error!");
+        }
     }
 }

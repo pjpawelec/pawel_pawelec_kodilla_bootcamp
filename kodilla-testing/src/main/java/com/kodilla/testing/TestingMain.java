1a44267db3142740e2f79ca4d68799788c9eec6c
pjpawelec <pj.pawelec@gmail.com>
2020-02-17 08:52:39 +0100

    6.1 commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,29 +5,5 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args){
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-        String result =  simpleUser.getUsername();
-
-        if(result.equals("theForumUser")) {
-            System.out.println("test OK");
-        }else{
-            System.out.println("Error!");
-        }
-
-        Calculator calculator = new Calculator();
-
-        int additionResult = calculator.add(3,2);
-        if(additionResult == 5) {
-            System.out.println("test dodawania OK");
-        }else{
-            System.out.println("dodawanie error!");
-        }
-
-        int subtractionResult = calculator.subtract(3,2);
-        if(subtractionResult == 1) {
-            System.out.println("test odejmowania OK");
-        }else{
-            System.out.println("odejmowanie error!");
-        }
-    }
+       }
 }

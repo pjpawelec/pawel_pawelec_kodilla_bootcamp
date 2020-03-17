c1a6089248dfa4bba4dde04ddbd3e4b7cf3f9079
pjpawelec <pj.pawelec@gmail.com>
2020-03-18 00:35:34 +0100

    8.3 commit

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderWithoutHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderWithoutHandling.java
@@ -8,23 +8,15 @@ import java.nio.file.Paths;
 import java.sql.SQLOutput;
 import java.util.stream.Stream;
 
-public class FileReader {
+public class FileReaderWithoutHandling {
 
-    public void readFile() throws FileReaderException {
+    public void readFile() throws IOException {
 
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
         Path path = Paths.get(file.getPath());
 
-        try {
-            Stream<String> fileLines = Files.lines(path);
-            fileLines.forEach(System.out::println);
-
-        } catch (IOException e) {
-            throw new FileReaderException();
-
-        } finally {
-            System.out.println("I am gonna be here...always!");
-        }
+        Stream<String> fileLines = Files.lines(path);
+        fileLines.forEach(System.out::println);
     }
 }
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,15 @@
+package com.kodilla.exception.test;
+
+import java.io.IOException;
+
+public class ExceptionHandling extends Exception {
+
+    public static void main(String args[]) {
+        try {
+            SecondChallenge secondChallenge = new SecondChallenge();
+            System.out.println(secondChallenge.probablyIWillThrowException(1.1,1.1));
+        } catch (ExceptionHandling e) {
+            System.out.println("Impossible to execute.");
+        }
+    }
+}
